29bb8bcf4a89d6ca064bf6e5050d4f010d9c7679
Kevin <kloklee888@gmail.com>
2020-05-26 05:11:10 -0400

    Added ngClass for winner bracket

--- src/app/bracket/full-tournament/full-tournament.component.ts
+++ src/app/bracket/full-tournament/full-tournament.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BracketService } from '../bracket.service';
-import { BracketOption } from '../bracket-option.model';
+import { BracketOption, Bracket } from '../bracket-option.model';
 
 @Component({
   selector: 'app-full-tournament',
@@ -9,6 +9,7 @@ import { BracketOption } from '../bracket-option.model';
 })
 export class FullTournamentComponent implements OnInit {
   @Input() fullBracket: BracketOption[][] = [];
+  @Input() finalchoice: BracketOption;
   bracketOptions: BracketOption[] = [];
   bracketTitle: string;
 
@@ -17,6 +18,17 @@ export class FullTournamentComponent implements OnInit {
   ngOnInit(): void {
     this.bracketTitle = this.bracketService.getBracketData().bracketTitle;
     this.fullBracket = this.bracketService.getFullBracket();
+  }
+
+  checkWinner(round: BracketOption[], i: number, j: number): Object {
+    if (this.fullBracket[i + 1] != undefined) {
+      if (this.fullBracket[i + 1].some(b => b.name === round[j].name)) {
+        return "winner";
+      }
+    } else if (this.finalchoice != null && this.finalchoice.name === round[j].name) {
+      return "winner";
+    }
+    return "";
   }
 
 }
