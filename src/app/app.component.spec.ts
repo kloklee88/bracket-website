8c14f369dc006bded7a227af084180bbe688f111
Kevin <kloklee888@gmail.com>
2020-06-15 18:31:56 -0400

    Renamed to bracket website

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kpop-bracket-website'`, () => {
+  it(`should have as title 'bracket-website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kpop-bracket-website');
+    expect(app.title).toEqual('bracket-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('kpop-bracket-website app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bracket-website app is running!');
   });
 });
