adaa70f82b12d5fa756da02f08f46cd4b87283d1
Kevin <kloklee888@gmail.com>
2021-06-17 00:53:53 -0400

    Developer tools construct JSON

--- src/app/bracket/bracket-option.model.ts
+++ src/app/bracket/bracket-option.model.ts
@@ -1,12 +1,31 @@
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
 export class Bracket {
   public bracketTitle: string;
   public bracketOptions: BracketOption[];
 }
 
 export class BracketOption {
-    public id: number;
-    public name: string;
-    public group: string;
-    public images: string[];
-    public imageUrl: string;
+  public id: number;
+  public name: string;
+  public group: string;
+  public images: string[];
+  public imageUrl: string;
+
+  static asFormGroup(bracketOption: BracketOption): FormGroup {
+    const fg = new FormGroup({
+      id: new FormControl(bracketOption.id, Validators.required),
+      name: new FormControl(bracketOption.name, Validators.required),
+      group: new FormControl(bracketOption.group, Validators.required),
+      images: new FormControl(bracketOption.images, Validators.required)
+    });
+    return fg;
+  }
+
+  constructor(id: number, name: string, group: string, images: string[]) {
+    this.id = id;
+    this.name = name;
+    this.group = group;
+    this.images = images;
+  }
 }
--- src/app/developer-tools/developer-tools.component.ts
+++ src/app/developer-tools/developer-tools.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { MatTable } from '@angular/material/table';
+import { BracketOption } from '../bracket/bracket-option.model';
 
 @Component({
   selector: 'app-developer-tools',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./developer-tools.component.css']
 })
 export class DeveloperToolsComponent implements OnInit {
+  form: FormGroup;
+  displayedColumns = ['name', 'group', 'images'];
+  @ViewChild(MatTable) table: MatTable<BracketOption>;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this._formBuilder.group({
+      bracketOptions: this._formBuilder.array([BracketOption.asFormGroup(new BracketOption(1, null, null, null))])
+    });
+    console.log(this.bracketOptions)
+    this.form.setControl('bracketOptions', this.bracketOptions);
+  }
+
+  get bracketOptions(): FormArray {
+    return this.form.get('bracketOptions') as FormArray;
+  }
+
+  onImagesInputChange(element: FormGroup) {
+    console.log("Images input changed");
+    let images = element.get("images");
+    images.setValue(images.value.split(","));
+  }
+
+  addData() {
+    this.bracketOptions.push(BracketOption.asFormGroup(new BracketOption(this.bracketOptions.length + 1, null, null, null)));
+    this.table.renderRows();
+  }
+
+  removeData() {
+    this.bracketOptions.removeAt(this.bracketOptions.length - 1);
+    this.table.renderRows();
   }
 
 }
